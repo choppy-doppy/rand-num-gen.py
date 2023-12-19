9c6b442f1769a10121091c6d79a3febd925682bb
Choppy <kaedongeddes@sd28.org>
2023-12-18 19:59:56 -0800

    basically done tbh

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import random
 
-def random_number():
+
+def generator():
+    user_input = input_gather()
+    random_number = random.randint(1, user_input)
+    return random_number
+
+
+def input_gather():
     while True:
         user_input = input("please enter a number: ")
+
+        if user_input == "exit":
+            quit(2)
 
         if user_input.isdigit():
             user_input = int(user_input)
@@ -9,11 +20,17 @@ def random_number():
             if user_input > 0:
                 break
             else:
-                print("currently the input needs 10 be greater than zero")
+                print("currently the input needs to be greater than zero")
 
         else:
-            print("invalid input")
+            print("invalid input!")
 
     return user_input
 
 
+def main():
+    random_number = generator()
+    print(random_number)
+
+
+main()
