f101e468a613957129650817c9968cf39ca55407
Choppy <kaedongeddes@sd28.org>
2023-12-18 19:26:35 -0800

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-while True:
-    rand_num = input("please type a number ")
-    print(rand_num)
+
+def random_number():
+    while True:
+        user_input = input("please enter a number: ")
+
+        if user_input.isdigit():
+            user_input = int(user_input)
+
+            if user_input > 0:
+                break
+            else:
+                print("currently the input needs 10 be greater than zero")
+
+        else:
+            print("invalid input")
+
+    return user_input
+
+
